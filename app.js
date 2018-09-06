16f0cc859be84d2228e05ed577628cf8487da29e
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2018-09-06 09:44:06 -0500

    Completed 'Callbacks' and 'Promises' sections

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
+/* INSTRUCTIONS */
+// Comment out line 7 for the Async exercises
+// Comment out line 28 for the Callbacks exercises
+// Comment out line XX for the Promises exercises
+// Comment out line XX for the Promises pt 2 exercises
+
 /*
 // Async: Now and later
 
+// Global assignment so we can return values into it
 let num;
 
 (function messageAndNumber() {
@@ -15,9 +22,10 @@ function productOfFour(n) {
 setTimeout(() => {
     productOfFour(num);
 }, 2000);
-*/
+/**/
 
 
+/*
 // Callbacks
 
 (function getWords() {
@@ -25,21 +33,61 @@ setTimeout(() => {
     let words = ["first", "fourth", "third", "second"];
 
     console.log(words[0]); // First
+
     setTimeout(() => {
         console.log(words[1]); // Fourth
     }, 3000);
+
     setTimeout(() => {
         console.log(words[2]); // Third
     }, 2000);
+
     setTimeout(() => {
         console.log(words[3]); // Second
     }, 1000);
 })();
 
-function countdown(num, callback) {
 
+function countdown(num, callback) {
+    if (num >= 1) {
+        console.log(num);
+        num--;
+        setTimeout(() => {
+            countdown(num, done); // https://www.google.com/search?q=recursion :)
+        }, 1000);
+        if (num === 0) {
+            callback();
+        }
+    }
 }
+
+countdown(5, done);
+
 
 function done() {
     console.log('We\'re done with the countdown!');
 }
+
+/**/
+
+// Promises
+let amIHungry = false;
+
+let orderChickenSandwich = new Promise((resolve, reject) => {
+    if (amIHungry) {
+        let sammie = {
+            sandwich: 'chicken',
+            veggies: 'lettuce'
+        };
+        resolve(sammie);
+    } else {
+        let notHungryError = new Error('You are not hungry at this time.');
+        reject(notHungryError);
+    }
+});
+
+(function orderFood() {
+    orderChickenSandwich
+        .then((sammie) => console.log(sammie))
+        .catch((notHungryError) => console.error(notHungryError));
+})();
