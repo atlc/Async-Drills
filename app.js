4cd47f57cb61192635ccf8c4aa42053d0f2d4f74
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2018-09-05 19:25:26 -0500

    Completed 'Async: Now and later' section and steps 1-5 of 'Callbacks' section

--- app.js
+++ app.js
@@ -1,3 +1,45 @@
-(function() {
+/*
+// Async: Now and later
 
+let num;
+
+(function messageAndNumber() {
+    console.log('Hello!');
+    return num = 5;
 })();
+
+function productOfFour(n) {
+    console.log(n*4);
+}
+
+setTimeout(() => {
+    productOfFour(num);
+}, 2000);
+*/
+
+
+// Callbacks
+
+(function getWords() {
+    // Will log words in ordinal fashion (logs 'first' ... 'fourth')
+    let words = ["first", "fourth", "third", "second"];
+
+    console.log(words[0]); // First
+    setTimeout(() => {
+        console.log(words[1]); // Fourth
+    }, 3000);
+    setTimeout(() => {
+        console.log(words[2]); // Third
+    }, 2000);
+    setTimeout(() => {
+        console.log(words[3]); // Second
+    }, 1000);
+})();
+
+function countdown(num, callback) {
+
+}
+
+function done() {
+    console.log('We\'re done with the countdown!');
+}
